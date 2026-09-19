c878783070fd385ae352b9ec336dd7b1cd27841a
Muhammed Arshad <muhammedarshad@pixbitsolutions.com>
2026-09-19 12:16:49 +0530

    fix: removed dummy contents

--- src/components/icons/SocialIcons.tsx
+++ src/components/icons/SocialIcons.tsx
@@ -11,33 +11,3 @@ export function InstagramIcon(props: IconProps) {
     </svg>
   );
 }
-
-export function FacebookIcon(props: IconProps) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path
-        d="M14 21v-7h2.5l.5-3H14V9c0-.9.3-1.5 1.7-1.5H17V4.8c-.3 0-1.3-.1-2.4-.1-2.4 0-4 1.5-4 4.1V11H8v3h2.6v7H14Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-export function XIcon(props: IconProps) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M5 5 19 19M19 5 5 19" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" />
-    </svg>
-  );
-}
-
-export function YoutubeIcon(props: IconProps) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <rect x="2.5" y="6" width="19" height="12" rx="4" stroke="currentColor" strokeWidth="1.7" />
-      <path d="M10.5 9.5 15 12l-4.5 2.5v-5Z" fill="currentColor" />
-    </svg>
-  );
-}
